062443c126ea79ced7953aaefc93431cc80519be
astur <astur@yandex.ru>
2018-05-09 07:13:28 +0300

    first tests

--- test.js
+++ test.js
@@ -1,7 +1,18 @@
 const test = require('ava');
-const m = require('.');
+const errres = require('.');
+const type = v => ({}).toString.call(v).slice(8, -1);
 
-test('errres', t => {
-    t.true(true);
-    t.is(m, m);
+test('type', t => {
+    t.is(type(errres()), 'Promise');
+});
+
+test('res', async t => {
+    t.is(await errres(null, 'TEST'), 'TEST');
+    t.is(await errres(), undefined);
+});
+
+test('err', async t => {
+    t.is(await errres('TEST').catch(e => e), 'TEST');
+    const err = await t.throws(errres('TEST'));
+    t.is(err, 'TEST');
 });
